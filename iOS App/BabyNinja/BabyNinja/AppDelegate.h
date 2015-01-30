b890ddd5ffda0744ceb89d0cefa8a5c64ac6bd57
snehaaggarwal <sneha.91089@gmail.com>
2015-01-29 19:56:21 -0600

    adding iOS app folder

---
+++ iOS App/BabyNinja/BabyNinja/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyNinja
+//
+//  Created by Sneha on 1/29/15.
+//  Copyright (c) 2015 TeamONE Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS App/BabyNinja/BabyNinja/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BabyNinja
+//
+//  Created by Sneha on 1/29/15.
+//  Copyright (c) 2015 TeamONE Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
