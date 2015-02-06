ecf2892b3b97dedbc257af98ec6a0ac749d04a98
snehaaggarwal <sneha.91089@gmail.com>
2015-02-06 16:03:36 -0600

    added the poop button functionality

--- iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/MainViewController.h
+++ iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/MainViewController.h
@@ -7,15 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "ALRadialMenu.h"
 
 #import "ICSDrawerController.h"
 
 
 
-@interface MainViewController : UIViewController<ICSDrawerControllerChild, ICSDrawerControllerPresenting>
+@interface MainViewController : UIViewController<ICSDrawerControllerChild, ICSDrawerControllerPresenting,ALRadialMenuDelegate>
 
 @property(nonatomic, weak) ICSDrawerController *drawer;
+
+@property (strong, nonatomic) ALRadialMenu *radialMenu;
+@property (weak, nonatomic) IBOutlet UIButton *poopButton;
 
 @end
 
--- iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/MainViewController.m
+++ iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/MainViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface MainViewController ()
 
@@ -32,8 +33,17 @@
     [self.openDrawerButton setImage:hamburger forState:UIControlStateNormal];
     [self.openDrawerButton addTarget:self action:@selector(openDrawer:) forControlEvents:UIControlEventTouchUpInside];
     
+    //create an instance of the radial menu and set ourselves as the delegate.
+    self.radialMenu = [[ALRadialMenu alloc] init];
+    self.radialMenu.delegate = self;
+    
     [self.view addSubview:self.openDrawerButton];
 }
+
+- (IBAction)poopButtonPressed:(id)sender {
+    [self.radialMenu buttonsWillAnimateFromButton:sender withFrame:self.poopButton.frame inView:self.view];
+}
+
 
 #pragma mark - Configuring the view’s layout behavior
 
@@ -66,4 +76,70 @@
     [self.drawer open];
 }
 
+#pragma mark - radial menu delegate methods
+- (NSInteger) numberOfItemsInRadialMenu:(ALRadialMenu *)radialMenu {
+    //FIXME: dipshit, change one of these variable names
+    if (radialMenu == self.radialMenu)
+        return 9;
+
+    
+    return 0;
+}
+
+
+- (NSInteger) arcSizeForRadialMenu:(ALRadialMenu *)radialMenu {
+    if (radialMenu == self.radialMenu)
+        return 360;
+
+    
+    return 0;
+}
+
+
+- (NSInteger) arcRadiusForRadialMenu:(ALRadialMenu *)radialMenu {
+//    if (radialMenu == self.radialMenu)
+        return 80;
+
+}
+
+
+- (ALRadialButton *) radialMenu:(ALRadialMenu *)radialMenu buttonForIndex:(NSInteger)index {
+    ALRadialButton *button = [[ALRadialButton alloc] init];
+    if (radialMenu == self.radialMenu) {
+        if (index == 1) {
+            [button setImage:[UIImage imageNamed:@"dribbble"] forState:UIControlStateNormal];
+        } else if (index == 2) {
+            [button setImage:[UIImage imageNamed:@"youtube"] forState:UIControlStateNormal];
+        } else if (index == 3) {
+            [button setImage:[UIImage imageNamed:@"vimeo"] forState:UIControlStateNormal];
+        } else if (index == 4) {
+            [button setImage:[UIImage imageNamed:@"pinterest"] forState:UIControlStateNormal];
+        } else if (index == 5) {
+            [button setImage:[UIImage imageNamed:@"twitter"] forState:UIControlStateNormal];
+        } else if (index == 6) {
+            [button setImage:[UIImage imageNamed:@"instagram500"] forState:UIControlStateNormal];
+        } else if (index == 7) {
+            [button setImage:[UIImage imageNamed:@"email"] forState:UIControlStateNormal];
+        } else if (index == 8) {
+            [button setImage:[UIImage imageNamed:@"googleplus-revised"] forState:UIControlStateNormal];
+        } else if (index == 9) {
+            [button setImage:[UIImage imageNamed:@"facebook500"] forState:UIControlStateNormal];
+        }
+        
+    }
+    
+    if (button.imageView.image) {
+        return button;
+    }
+    
+    return nil;
+}
+
+
+- (void) radialMenu:(ALRadialMenu *)radialMenu didSelectItemAtIndex:(NSInteger)index {
+    if (radialMenu == self.radialMenu) {
+        [self.radialMenu itemsWillDisapearIntoButton:self.poopButton];
+    }
+    
+}
 @end
