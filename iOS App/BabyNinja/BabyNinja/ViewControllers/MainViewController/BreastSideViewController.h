5e52bcdaa72be5fa246d88e788dea1e91dd2f20e
snehaaggarwal <sneha.91089@gmail.com>
2015-04-27 11:49:33 -0500

    feed UI control basic screens added

---
+++ iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/BreastSideViewController.h
@@ -0,0 +1,13 @@
+//
+//  BreastSideViewController.h
+//  BabyNinja
+//
+//  Created by Sneha on 4/27/15.
+//  Copyright (c) 2015 TeamONE Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BreastSideViewController : UIViewController
+
+@end
---
+++ iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/FeedOuncesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedOuncesViewController.h
+//  BabyNinja
+//
+//  Created by Sneha on 4/27/15.
+//  Copyright (c) 2015 TeamONE Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedOuncesViewController : UIViewController
+
+@end
