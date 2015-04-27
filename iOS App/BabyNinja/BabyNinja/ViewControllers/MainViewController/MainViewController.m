9c69b6e1f25377e6a632449c76e9ef4baf239f72
shkkim <shk.kim@gmail.com>
2015-04-27 15:26:04 -0500

    working on PEE

--- iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/MainViewController.m
+++ iOS App/BabyNinja/BabyNinja/ViewControllers/MainViewController/MainViewController.m
@@ -121,27 +121,27 @@
             NSLog(@"NOT DONE");        }
     }];
     
-    NSLog(@"RETRIVE^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-    PFQuery *query = [PFQuery queryWithClassName:@"CareTaker"];
-//    [query whereKey:@"playerName" equalTo:@"Dan Stemkoski"];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            // The find succeeded.
-            //NSLog(@"Successfully retrieved %d scores.", objects.count);
-            // Do something with the found objects
-            for (PFObject *object in objects) {
-                CareTaker *here = object;
-                Baby *baby2 =  here.careTakerBabyArray[0];
-                NSLog(@"%@", baby2);
-              
-                
-                
-            }
-        }else{
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
+//    NSLog(@"RETRIVE^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+//    PFQuery *query = [PFQuery queryWithClassName:@"CareTaker"];
+////    [query whereKey:@"playerName" equalTo:@"Dan Stemkoski"];
+//    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+//        if (!error) {
+//            // The find succeeded.
+//            //NSLog(@"Successfully retrieved %d scores.", objects.count);
+//            // Do something with the found objects
+//            for (PFObject *object in objects) {
+//                CareTaker *here = object;
+//                Baby *baby2 =  here.careTakerBabyArray[0];
+//                NSLog(@"%@", baby2);
+//              
+//                
+//                
+//            }
+//        }else{
+//            // Log details of the failure
+//            NSLog(@"Error: %@ %@", error, [error userInfo]);
+//        }
+//    }];
     
 }
 
