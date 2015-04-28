4c97b1cea0a4772181bf7cfe587e141d9b234472
snehaaggarwal <sneha.91089@gmail.com>
2015-04-28 10:09:35 -0500

    minor changes in the watch app

---
+++ iOS App/BabyNinja/BabyNinja WatchKit Extension/WatchConstants.m
@@ -0,0 +1,17 @@
+//
+//  Constants.m
+//  BabyNinja
+//
+//  Created by Sneha on 4/16/15.
+//  Copyright (c) 2015 TeamONE Software. All rights reserved.
+//
+
+#import "WatchConstants.h"
+
+NSString *const UDefaultLoggedIn = @"UDefaultLoggedIn";
+NSString *const ULogoutNotification = @"ULogoutNotification";
+
+NSString *const AppGroupFile = @"group.TOS.BabyNinja";
+NSString *const DiaperCount = @"DiaperCount";
+NSString *const MinDiaperCount = @"MinDiaperCount";
+NSString *const NewDiaperCount = @"newDiaperCount";
---
+++ iOS App/BabyNinja/BabyNinja WatchKit Extension/WatchUtility.m
@@ -0,0 +1,38 @@
+//
+//  Utility.m
+//  BabyNinja
+//
+//  Created by Sneha on 4/27/15.
+//  Copyright (c) 2015 TeamONE Software. All rights reserved.
+//
+
+#import "WatchUtility.h"
+#import "WatchConstants.h"
+
+@implementation WatchUtility
+
++ (id)sharedUtility {
+    
+    static WatchUtility *utility = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        utility = [[self alloc] init];
+    });
+    return utility;
+}
+
+
+-(void)saveUserDefaultObject:(id)object forKey:(NSString*)key{
+    NSUserDefaults *myDefaults = [[NSUserDefaults alloc]
+                                  initWithSuiteName:AppGroupFile];
+    [myDefaults setObject:object forKey:key];
+}
+
+-(id)userDefaultForKey:(NSString*)key{
+    NSUserDefaults *myDefaults = [[NSUserDefaults alloc]
+                                  initWithSuiteName:AppGroupFile];
+    
+    return [myDefaults objectForKey:key];
+}
+
+@end
