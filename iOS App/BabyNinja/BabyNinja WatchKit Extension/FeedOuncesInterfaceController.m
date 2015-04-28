02d0370564922393ba5c4abc04425117c2a07714
snehaaggarwal <sneha.91089@gmail.com>
2015-04-28 11:34:49 -0500

    feed app comm recorded

--- iOS App/BabyNinja/BabyNinja WatchKit Extension/FeedOuncesInterfaceController.m
+++ iOS App/BabyNinja/BabyNinja WatchKit Extension/FeedOuncesInterfaceController.m
@@ -34,6 +34,49 @@
     [super didDeactivate];
 }
 
+- (IBAction)twoOzSelected {
+    [self recordFeedWithOz:[NSNumber numberWithInt:2]];
+}
+
+- (IBAction)fiveOzSelected {
+    [self recordFeedWithOz:[NSNumber numberWithInt:5]];
+
+}
+
+- (IBAction)tenOzSelected {
+    [self recordFeedWithOz:[NSNumber numberWithInt:10]];
+
+}
+
+- (IBAction)fifteenOzSelected {
+    [self recordFeedWithOz:[NSNumber numberWithInt:15]];
+
+}
+
+- (IBAction)twentyOzSelected {
+    [self recordFeedWithOz:[NSNumber numberWithInt:20]];
+
+}
+
+
+-(void)recordFeedWithOz:(NSNumber*)oz{
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    [dict setObject:@"feed" forKey:@"activity"];
+    [dict setObject:@"oz" forKey:@"type"];
+    [dict setObject:oz forKey:@"quantity"];
+    
+    [FeedOuncesInterfaceController openParentApplication:dict reply:^(NSDictionary *replyInfo, NSError *error) {
+        
+        if (error) {
+            NSLog(@"%@", error);
+        } else {
+            NSLog(@"%@",replyInfo);
+        }
+        
+    }];
+    
+    [self popController];
+}
 @end
 
 
